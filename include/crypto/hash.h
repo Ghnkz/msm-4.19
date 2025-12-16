8451bd1fe02112cbd32e474bfb0b23a6a3fb4492
Stephan Mueller <smueller@chronox.de>
2025-12-16 17:49:42 +0700

    LRNG: Backport to 4.19 kernel
    From: https://github.com/smuellerDD/lrng/tree/v57/backports/v57-4.19.322
    
    Co-authored-by: wulan17 <wulan17@komodos.id>
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- include/crypto/hash.h
+++ include/crypto/hash.h
@@ -151,9 +151,8 @@ struct shash_desc {
 	void *__ctx[] CRYPTO_MINALIGN_ATTR;
 };
 
-#define HASH_MAX_DIGESTSIZE	 64
-#define HASH_MAX_DESCSIZE	360
-#define HASH_MAX_STATESIZE	512
+
+#define HASH_MAX_DESCSIZE	(sizeof(struct shash_desc) + 360)
 
 #define SHASH_DESC_ON_STACK(shash, ctx)				  \
 	char __##shash##_desc[sizeof(struct shash_desc) +	  \
