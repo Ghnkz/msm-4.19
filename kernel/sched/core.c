009e86d1ddf065012b025c37006c34c71a0e9881
Ghnkz <49773907+Ghnkz@users.noreply.github.com>
2026-08-19 17:40:46 +0700

    Fixup!! FROMLIST: mm: multi-gen LRU: support page table walks

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3750,8 +3750,12 @@ static __always_inline struct rq *
 context_switch(struct rq *rq, struct task_struct *prev,
 	       struct task_struct *next, struct rq_flags *rf)
 {
+	struct mm_struct *mm, *oldmm;
+
 	prepare_task_switch(rq, prev, next);
 
+	mm = next->mm;
+	oldmm = prev->active_mm;
 	/*
 	 * For paravirt, this is coupled with an exit in switch_to to
 	 * combine the page table reload and the switch backend into
@@ -3766,37 +3770,19 @@ context_switch(struct rq *rq, struct task_struct *prev,
 	 * kernel ->   user   switch + mmdrop() active
 	 *   user ->   user   switch
 	 */
-	if (!mm) {
+	if (!mm) {                                      // to kernel
 		next->active_mm = oldmm;
 		mmgrab(oldmm);
 		enter_lazy_tlb(oldmm, next);
-	} else {
+	} else {                                        // to user
 		switch_mm_irqs_off(oldmm, mm, next);
 		lru_gen_use_mm(mm);
 	}
 
-		next->active_mm = prev->active_mm;
-		if (prev->mm)                           // from user
-			mmgrab(prev->active_mm);
-		else
-			prev->active_mm = NULL;
-	} else {                                        // to user
-		/*
-		 * sys_membarrier() requires an smp_mb() between setting
-		 * rq->curr and returning to userspace.
-		 *
-		 * The below provides this either through switch_mm(), or in
-		 * case 'prev->active_mm == next->mm' through
-		 * finish_task_switch()'s mmdrop().
-		 */
-
-		switch_mm_irqs_off(prev->active_mm, next->mm, next);
-
-		if (!prev->mm) {                        // from kernel
-			/* will mmdrop() in finish_task_switch(). */
-			rq->prev_mm = prev->active_mm;
-			prev->active_mm = NULL;
-		}
+	if (!prev->mm) {                                // from kernel
+		/* will mmdrop() in finish_task_switch(). */
+		rq->prev_mm = oldmm;
+		prev->active_mm = NULL;
 	}
 
 	rq->clock_update_flags &= ~(RQCF_ACT_SKIP|RQCF_REQ_SKIP);
